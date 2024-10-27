e9f1410c91b564aeb1b04cec432abc04f2a68342
Halanau327 <a.halanau@gmail.com>
2024-10-27 14:46:43 +0300

    update: refactoring google game

---
+++ game.js
@@ -0,0 +1,50 @@
+export class Game {
+    #state;
+    #googlePosition;
+    #numberUtil;
+    #settings
+
+
+    // dependency injection
+    constructor(numberUtil) {
+        this.#state = GAME_STATUSES.PENDING;
+        this.#googlePosition = {x: 1, y: 1}
+        this.#numberUtil = numberUtil
+        this.#settings = {
+            gridSize: {
+                columnCount: 1,
+                rowsCount: 2
+            },
+            jumpInterval: 10
+        }
+    }
+
+    #jumpGoogle() {
+
+        const newGooglePosition = {
+            x: this.#numberUtil.getRandomNumber(0, this.#settings.gridSize.x),
+            y: this.#numberUtil.getRandomNumber(0, this.#settings.gridSize.y),
+        }
+    }
+    1
+
+
+    async getStatus() {
+        return this.#state
+    }
+
+    async start() {
+        setInterval(this.#jumpGoogle.bind(this), 2000)
+        this.#state = GAME_STATUSES.IN_PROGRESS
+    }
+
+    async getGooglePosition() {
+        return this.#googlePosition
+    }
+}
+
+export const GAME_STATUSES = {
+    PENDING: 'PENDING',
+    IN_PROGRESS: 'IN-PROGRESS',
+    FINISHED: 'FINISHED'
+}
